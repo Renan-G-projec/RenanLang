1fb134801cc75ff59abf9db53c9b5cea365e38c7
Renan-G-projec <renanfartes@gmail.com>
2026-07-26 19:12:10 -0300

    feat: error handling changed to remove try-catch. AMDD

--- include/virtualMachine.hpp
+++ include/virtualMachine.hpp
@@ -6,13 +6,6 @@
 #include <stack>
 #include <iostream>
 
-struct Error {
-    Error(const std::string message, std::uint32_t address);
-
-    const std::string message;
-    std::uint32_t address;
-};
-
 class VirtualMachine {
 public:
     void loadBytecode(std::int8_t* bytecode);
--- src/virtualMachine.cpp
+++ src/virtualMachine.cpp
@@ -2,8 +2,6 @@
 #include "virtualMachine.hpp"
 #include "opcodeTable.hpp"
 
-Error::Error(const std::string message, std::uint32_t address) : message{message}, address{address} {};
-
 void VirtualMachine::loadBytecode(std::int8_t* bytecode) {
     this->mCode = bytecode;
 }
@@ -24,37 +22,41 @@ void VirtualMachine::_fetch() {
 }
 
 void VirtualMachine::_execute() {
-    try {
-        switch (mCurrentInstruction[0]) {
-            case Opcode::PUSH: {
-                mStack.push(mCurrentInstruction[1]);
-                break;
-            }
-            case Opcode::ADD: {
-                int arg1 = popStack();
-                int arg2 = popStack();
-                int sum = arg1 + arg2;
-                mStack.push(sum);
-                break;
-            }
-            case Opcode::PRINT: {
-                int numberToPrint = popStack();
-                std::cout << numberToPrint << '\n';
-                break;
-            }
-            case Opcode::HALT: {
+    switch (mCurrentInstruction[0]) {
+        case Opcode::PUSH: {
+            mStack.push(mCurrentInstruction[1]);
+            break;
+        }
+        case Opcode::ADD: {
+            if (mStack.size() < 2) {
+                std::cout << "Error: At line " << mCurrentAddress << '\n' << "Opcode ADD: Stack Underflow";
                 mRunning = false;
                 break;
-            }
+            } 
+            int arg1 = popStack();
+            int arg2 = popStack();
+            int sum = arg1 + arg2;
+            mStack.push(sum);
+            break;
         }
-    } catch (Error error) {
-        std::cout << "ERROR: at address " << error.address << '\n' << error.message << '\n';
-        mRunning = false;
+        case Opcode::PRINT: {
+            if (mStack.size() < 1) {
+                std::cout << "Error: At line " << mCurrentAddress << '\n' << "Opcode PRINT: Stack Underflow";
+                mRunning = false;
+                break;
+            } 
+            int numberToPrint = popStack();
+            std::cout << numberToPrint << '\n';
+            break;
+        }
+        case Opcode::HALT: {
+            mRunning = false;
+            break;
+        }
     }
 }
 
 std::int8_t VirtualMachine::popStack() {
-    if (mStack.size() < 1) throw Error("Empty stack cannot be popped.", mCurrentAddress);
     std::int8_t stackTop = mStack.top();
     mStack.pop();
     return stackTop;
