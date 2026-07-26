08e6929885a086252ff5e91ccf652a244703bcbc
Renan-G-projec <renanfartes@gmail.com>
2026-07-26 09:47:22 -0300

    feat: Added RenanLang module pseudo-implementation. AMDG

---
+++ src/renanLang.cpp
@@ -0,0 +1,14 @@
+// Ad Maiorem Dei Gloriam!
+#include "renanLang.hpp"
+
+void RenanLang::loadFile(const char *fileName) {
+    // mFileLoader.load(mLoadedCode);
+    // mCompiler.compile(mCompiledCode, mLoadedCode);
+    return;
+}
+
+void RenanLang::init() {
+    // mVM.loadBytecode(mCompiledCode);
+    // mVM.init();
+    return;
+}
