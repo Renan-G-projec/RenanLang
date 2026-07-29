4afb3b01900dc021e888b82f483ab0e76e7f320d
Renan-G-projec <renanfartes@gmail.com>
2026-07-28 23:57:18 -0300

    feat: Lexer majorlly implemented. AMDG

--- include/compiler/lexer.hpp
+++ include/compiler/lexer.hpp
@@ -4,45 +4,50 @@
 
 #include <string>
 #include <vector>
-#include <sstream>
+#include <string>
 #include <iostream>
 #include <optional>
 
 enum Token {
     KEYWORD,
     LABEL,
-    LITERAL,
+    LABEL_IDENTIFIER,
+    STRING_LITERAL,
+    CHAR_LITERAL,
+    NUM_LITERAL,
     SEPARATOR
 };
 
-enum EvaluateToken {
-
-};
-
-typedef std::vector<std::pair<EvaluateToken, std::optional<std::string>>> lexer_output_t;
+typedef std::vector<std::pair<Token, std::string>> scanned_code_t;
 
 class Lexer {
 public:
-    bool tokenize(const std::string& rawCode, lexer_output_t& tokenContainer);
-    std::string getError();
+    bool tokenize(const std::string& rawCode, scanned_code_t& tokenContainer);
+    const std::string& getError();
+
+    void printLexerOutput(scanned_code_t scannedCode);
 
 private:
-    typedef std::vector<std::pair<Token, std::string>> scanned_code_t;
     scanned_code_t mScannedCode;
 
     bool mRunning = true;
     std::string errorMessage = "";
 
+    unsigned int mCurrentPos = 0;
+
     Token getToken(const std::string& token);
 
     void scanCode(const std::string& code);
-    void evaluateCode(lexer_output_t& output);
 
-    bool isComment(const std::string& token);
-    bool isKeyword(const std::string& token);
-    bool isLabel(const std::string& token);
-    bool isLiteral(const std::string& token);
-    bool isSeparator(const std::string& token);
+
+
+    // Helper functions
+    bool isWhitespace(char);
+    bool isNewline(char);
+    bool isAlpha(char);
+    bool isDigit(char);
+    bool isAlphanumeric(char);
+    bool isKeyword(const std::string&);
 };
 
 #endif
--- src/compiler/lexer.cpp
+++ src/compiler/lexer.cpp
@@ -1,15 +1,150 @@
 // Ad Maiorem Dei Gloriam!
 #include "compiler/lexer.hpp"
 
-bool Lexer::tokenize(const std::string& rawCode, lexer_output_t& tokenContainer) {
-    std::stringstream codeStream{rawCode};
+bool Lexer::tokenize(const std::string& rawCode, scanned_code_t& tokenContainer) {
     std::string rawToken;
     
     scanCode(rawCode);
-    evaluateCode(tokenContainer);
-    return true;
+    if (mRunning) {
+        tokenContainer = mScannedCode;
+        return true;
+    } else {
+        std::cout << "ERROR: During lexical analisys: " << getError() << '\n';
+        return false;
+    }
+}
+
+void Lexer::scanCode(const std::string& rawCode) {
+    mCurrentPos = 0;
+
+    enum State {
+        NONE,
+        COMMENT,
+        STRING_LITERAL,
+        CHAR_LITERAL
+    };
+
+    State currentState = NONE;
+    while (mCurrentPos < rawCode.length() && mRunning) {
+        const char currentChar = rawCode[mCurrentPos];
+
+        switch (currentState) {
+            case State::NONE: {
+                // Skiping blank characters
+                if (isWhitespace(currentChar)) break;
+                if (isAlpha(currentChar)) {
+                    std::string alphanumericContent{currentChar};
+                    while (isAlphanumeric(rawCode[++mCurrentPos])) alphanumericContent += rawCode[mCurrentPos];
+                    if (isKeyword(alphanumericContent)) {
+                        mScannedCode.push_back({Token::KEYWORD, alphanumericContent});
+                        break;
+                    } else {
+                        mScannedCode.push_back({rawCode[++mCurrentPos] == ':' ? Token::LABEL : Token::LABEL_IDENTIFIER, alphanumericContent});
+                    }
+
+                    if (!isWhitespace(rawCode[mCurrentPos])) {
+                        mRunning = false;
+                        errorMessage = alphanumericContent + " is not a valid label name.";
+                    }
+                } else if (isDigit(currentChar)) {
+                    std::string numberLiteralContent{currentChar};
+                    while (!isWhitespace(rawCode[++mCurrentPos])) {
+                        if (!isDigit(rawCode[mCurrentPos])) {
+                            mRunning = false;
+                        }
+                        numberLiteralContent += rawCode[mCurrentPos];
+                    }
+                    if (!mRunning) errorMessage = numberLiteralContent + " is not a recognized number literal.";
+                    mScannedCode.push_back({Token::NUM_LITERAL, numberLiteralContent});
+                } else if (currentChar == '\'') {
+                    currentState = CHAR_LITERAL;
+                } else if (currentChar == '"') {
+                    currentState = STRING_LITERAL;
+                    mCurrentPos--;
+                } else if (currentChar == ';') {
+                    mScannedCode.push_back({Token::SEPARATOR, ";"});
+                }
+                break;
+            }
+            case State::COMMENT: {
+                while (!isNewline(rawCode[++mCurrentPos]));
+                currentState = NONE;
+                break; 
+            }
+            case State::STRING_LITERAL: {
+                std::string strLiteral;
+                while (rawCode[++mCurrentPos] != '"') strLiteral += rawCode[mCurrentPos];
+                mScannedCode.push_back({Token::STRING_LITERAL, strLiteral});
+                currentState = NONE;
+
+                break;
+            }
+            case State::CHAR_LITERAL: {
+                char ch = rawCode[mCurrentPos++];
+                mScannedCode.push_back({Token::CHAR_LITERAL, std::string{ch}});
+                currentState = NONE;
+
+                break;
+            }
+        }
+        mCurrentPos++; // Scapes trailing character such as \n ' and "
+    }
+}
+
+bool Lexer::isWhitespace(char ch) {
+    return ch == '\n' || ch == ' ' || ch == '\r' || ch == '\0' || ch == '\t';
+}
+
+bool Lexer::isNewline(char ch) {
+    return ch == '\n';
+}
+
+bool Lexer::isAlpha(char ch) {
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+}
+
+bool Lexer::isDigit(char ch) {
+    return ch >= '0' && ch <= '9';
+}
+
+bool Lexer::isAlphanumeric(char ch) {
+    return isDigit(ch) || isAlpha(ch);
+}
+
+bool Lexer::isKeyword(const std::string& token) {
+    return
+        token == "PUSH"                 || token == "PRINT"       || token == "PRINT_ASCII" ||
+        token == "JMP"                  || token == "JMP_IF_ZERO" || token == "JMP_LABEL"   ||
+        token == "JMP_IF_ZERO_LABEL"    || token == "HALT";
+}
+
+const std::string& Lexer::getError() { 
+    return errorMessage;
 }
 
 #ifdef DEBUG
+
+std::string getTokenTypeName(Token token) {
+    switch (token) {
+        case Token::KEYWORD: 
+            return "KEYWORD";
+        case Token::LABEL:
+            return "LABEL";
+        case Token::LABEL_IDENTIFIER:
+            return "LABEL_IDENTIFIER";
+        case Token::CHAR_LITERAL:
+            return "CHAR_LITERAL";
+        case Token::NUM_LITERAL:
+            return "NUM_LITERAL";
+        case Token::STRING_LITERAL:
+            return "STRING_LITERAL";
+        case Token::SEPARATOR:
+            return "SEPARATOR";
+    }
+}
+
+void Lexer::printLexerOutput(scanned_code_t scannedCode) {
+    for (auto token : scannedCode) std::cout << "Type: " << getTokenTypeName(token.first) << " Value:" << token.second << '\n';
+}
 
 #endif
