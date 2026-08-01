5bf996de95fc2d6891958425ca4c34f1317d9cd6
Renan-G-projec <renanfartes@gmail.com>
2026-08-01 12:07:27 -0300

    feat: File not exist edge case treated. AMDG

--- src/fileLoader.cpp
+++ src/fileLoader.cpp
@@ -6,6 +6,12 @@ void FileLoader::load(const std::string& filename, std::string& target) const {
     std::fstream fileStream(filename, std::ios::in);
     std::string buffer;
 
+    if (!fileStream.is_open()) {
+        std::cout << "Fatal error: " << filename << " not found.\n";
+        std::cout << "Exiting the program.\n";
+        exit(0);
+    }
+
     while (std::getline(fileStream, buffer)) {
         target += buffer + '\n';
     }
